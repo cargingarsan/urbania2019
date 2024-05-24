4324a93a4c09546e8cf3ff74c361dcc1787a47cc
cargingarsan <cargingarsan@gmail.com>
2024-05-23 20:27:25 -0500

    creacion de la carpeta y proyecto en R

---
+++ lima_inmobliaria_2019.R
@@ -0,0 +1,21 @@
+#usethis::create_from_github(
+#  "https://github.com/cargingarsan/urbania2019.git",
+#  destdir = ("E:/proyectos/rstudio_git/ml")
+#)
+
+
+
+
+library(tidyverse)
+library(leaflet)
+#library()
+library(collapse)
+
+
+lima_urb <- read_csv("E:\\proyectos\\rstudio_git\\ml\\urbania2019\\data\\archive.csv.zip")
+                     
+
+
+
+#sep=",")
+
